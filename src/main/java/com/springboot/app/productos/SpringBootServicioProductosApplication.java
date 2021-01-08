107e43a0f11c29a60b930a7cb230a6e7b6b30b7b
luis576 <pedroluisbarrosvalle@hotmail.com>
2021-01-08 13:01:19 -0500

    Creando nuestro microservicio productos y su clase Entity

---
+++ src/main/java/com/springboot/app/productos/SpringBootServicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioProductosApplication.class, args);
+	}
+
+}
