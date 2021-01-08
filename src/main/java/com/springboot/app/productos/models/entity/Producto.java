83b27a2281b469ed6d4a892ae8e55335291b6d3e
luis576 <pedroluisbarrosvalle@hotmail.com>
2021-01-08 13:51:28 -0500

    Creando el controlador rest e insertando datos de prueba

--- src/main/java/com/springboot/app/productos/models/entity/Producto.java
+++ src/main/java/com/springboot/app/productos/models/entity/Producto.java
@@ -17,25 +17,57 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "productos")
-public class Producto implements Serializable{	
+public class Producto implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nombre;
-	
+
 	private Double precio;
 
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	private static final long serialVersionUID = -206243428079519635L;
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre,precio,create_at) VALUES ('Panasonic',800,NOW());
+INSERT INTO productos (nombre,precio,create_at) VALUES ('LG',954,NOW());
+INSERT INTO productos (nombre,precio,create_at) VALUES ('SAMSUNG',670,NOW());
+INSERT INTO productos (nombre,precio,create_at) VALUES ('LENOVO',520,NOW());
+INSERT INTO productos (nombre,precio,create_at) VALUES ('HP',1800,NOW());
+INSERT INTO productos (nombre,precio,create_at) VALUES ('LOGITECH',120,NOW());
